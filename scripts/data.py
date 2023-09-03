cb127ff0c219cd708f40370323fd4d3db83533ad
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-09-03 18:34:19 -0300

    created function to load unbalanced german

--- scripts/data.py
+++ scripts/data.py
@@ -5,3 +5,13 @@ def load_german():
     X=df.drop(["Risk"],axis=1)
     Y=df["Risk"]
     return X, Y
+
+def load_unbalanced_german():
+    df = pd.read_csv("../data/german_credit_data_K_preprocessed.csv")
+    df = pd.concat([
+        df[df.Sex == 0],
+        df[df.Sex == 1].sample(100)
+    ]).reset_index(drop = True)
+    X=df.drop(["Risk"],axis=1)
+    Y=df["Risk"]
+    return X, Y
