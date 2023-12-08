574ff99b2f70a199777605ce5b7f6892a0386ddb
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-12-07 22:34:36 -0300

    small change to taiwan data loader

--- scripts/data.py
+++ scripts/data.py
@@ -10,7 +10,17 @@ def load_taiwan():
     df = pd.read_csv("../data/taiwan_preprocessed.csv")
     X = df.drop(["DEFAULT"], axis = 1)
     Y = df["DEFAULT"]
+    X.insert(0, "Sex", X["SEX_Female"])
+    X = X.drop(columns = ["SEX_Female", "SEX_Male"])
     return X, Y
+
+def load_dataset(dataset):
+    if dataset == "german":
+        return load_german()
+    elif dataset == "taiwan":
+        return load_taiwan()
+    else:
+        raise ValueError(f"Unknown dataset {dataset}")
 
 def load_unbalanced_german():
     df = pd.read_csv("../data/german_credit_data_K_preprocessed.csv")
