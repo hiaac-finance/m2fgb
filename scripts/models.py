1ed32b4d69893866064340423300461c1c4aad42
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2024-01-11 12:48:50 -0300

    add version of xtremefair used for testing

--- scripts/models.py
+++ scripts/models.py
@@ -449,6 +449,305 @@ class XtremeFair(BaseEstimator, ClassifierMixin):
                 "Invalid return_type. Choose 'combined', 'performance', or 'fairness'."
             )
 
+def dual_obj_1(
+    subgroup,
+    fair_weight,
+    group_losses,
+    mu_opt_list,
+    fairness_constraint="equalized_loss",
+    dual_learning="optim",
+    multiplier_learning_rate=0.1,
+):
+    """This helper function will define a custom objective function for XGBoost using the fair_weight parameter.
+
+    Parameters
+    ----------
+    soubgroup : ndarray
+        Array with the subgroup labels.
+    fair_weight : float
+        Weight of the fairness term in the loss function.
+    group_losses : list
+        List where the losses for each subgroup will be stored.
+    mu_opt_list: list
+        List where the optimal mu for each subgroup will be stored.
+    fairness_constraint: str, optional
+        Fairness constraint used in learning.
+    dual_learning : str, optional
+        Method used to learn the dual problem, by default "optim"
+    multiplier_learning_rate: float, optional
+        Learning rate used in the gradient learning of the dual, used only if dual_learning="gradient", by default 0.1
+    """
+    #mu_opt_list = [None]
+    n = len(subgroup)
+    n_g = get_subgroup_indicator(subgroup)
+    #print("n_g", n_g, n_g.max(axis = 0))
+
+    def custom_obj(predt, dtrain):
+        loss_group = logloss_group(predt, dtrain, subgroup, fairness_constraint)
+        group_losses.append(loss_group)
+        if fair_weight > 0:
+            if dual_learning == "optim":
+                # dual problem solved analytically
+                idx_biggest_loss = np.where(loss_group == np.max(loss_group))[0]
+                # if is more than one, randomly choose one
+                idx_biggest_loss = np.random.choice(idx_biggest_loss)
+                mu_opt = np.zeros(loss_group.shape[0])
+                mu_opt[idx_biggest_loss] = fair_weight
+                if mu_opt_list[0] is None:
+                    mu_opt_list[0] = mu_opt
+                else:
+                    mu_opt_list.append(mu_opt)
+
+            elif dual_learning == "gradient":
+                if mu_opt_list[0] is None:
+                    mu_opt = np.zeros(loss_group.shape[0])
+                    # mu_opt = mu_opt / np.sum(mu_opt) * fair_weight
+                    mu_opt_list[0] = mu_opt
+
+                else:
+                    mu_opt = mu_opt_list[-1].copy()
+                    mu_opt += multiplier_learning_rate * fair_weight * loss_group
+                    mu_opt_list.append(mu_opt)
+
+        else:
+            mu_opt = np.zeros(len(np.unique(subgroup)))
+            if mu_opt_list[0] is None:
+                mu_opt_list[0] = mu_opt
+            else:
+                mu_opt_list.append(mu_opt)
+
+
+        grad = logloss_grad(predt, dtrain) / n
+        hess = logloss_hessian(predt, dtrain) / n
+
+        #print("grad:", grad)
+        #print("hess:", hess)
+
+        #print("logloss_group_grad", (logloss_group_grad(predt, dtrain, subgroup, fairness_constraint) * n_g @ mu_opt))
+        if fair_weight > 0:
+            grad += (
+                logloss_group_grad(predt, dtrain, subgroup, fairness_constraint)
+                * n_g
+                @ mu_opt
+            )
+            hess += (
+                logloss_group_hess(predt, dtrain, subgroup, fairness_constraint)
+                * n_g
+                @ mu_opt
+            )
+
+        grad *= n / (1 + fair_weight)
+        hess *= n / (1 + fair_weight)
+        return grad, hess
+
+    return custom_obj
+
+
+
+class XtremeFair_1(BaseEstimator, ClassifierMixin):
+    """Classifier that modifies XGBoost to incorporate fairness into the loss function.
+    The scoring is performed with a weighted sum between accuracy and a fairness metric.
+    The alpha parameter controls the weight, alpha=1 means only accuracy is considered, alpha=0 means only fairness is considered.
+    It shares many of the parameters with XGBoost to control learning and decision trees.
+    Currently it is only able to incorporate "equalized_loss" as a fairness constraint and "EOD" as a fairness metric.
+
+
+    Parameters
+    ----------
+    fairness_constraint : str, optional
+        Fairness constraint used in learning, currently only supports "equalized_loss", by default "equalized_loss"
+    fair_weight : int, optional
+        Weight for fairness in loss formulation, by default 1
+    dual_learning : str, optional
+        Method used to learn the dual problem, by default "optim"
+    multiplier_learning_rate: float, optional
+        Learning rate used in the gradient learning of the dual, used only if dual_learning="gradient", by default 0.1
+    n_estimators : int, optional
+        Number of estimators used in XGB, by default 10
+    eta : float, optional
+        Learning rate of XGB, by default 0.3
+    colsample_bytree : float, optional
+        Size of sample of of the columns used in each estimator, by default 1
+    max_depth : int, optional
+        Max depth of decision trees of XGB, by default 6
+    min_child_weight : int, optional
+        Weight used to choose partition of tree nodes, by default 1
+    max_leaves : int, optional
+        Max number of leaves of trees, by default 0
+    l2_weight : int, optional
+        Weight of L2 regularization, by default 1
+    alpha : int, optional
+        Weight used for the score function of the method, by default 1
+    performance_metric : str, optional
+        Performance metric used by model score, supports ["accuracy", "auc"], by default "accuracy"
+    fairness_metric : str, optional
+        Fairness metric used by model score, supports ["SPD", "EOP"] by default "EOP"
+    random_state : int, optional
+        Random seed used in learning, by default None
+    """
+
+    def __init__(
+        self,
+        fairness_constraint="equalized_loss",
+        fair_weight=1,
+        dual_learning="optim",
+        multiplier_learning_rate=0.1,
+        n_estimators=10,
+        eta=0.3,
+        colsample_bytree=1,
+        max_depth=6,
+        min_child_weight=1,
+        max_leaves=0,
+        l2_weight=1,
+        alpha=1,
+        performance_metric="accuracy",
+        fairness_metric="EOP",
+        random_state=None,
+    ):
+        assert fairness_constraint in [
+            "equalized_loss",
+            "equal_opportunity",
+            "demographic_parity",
+        ]
+        assert dual_learning in ["optim", "gradient"]
+        assert performance_metric in ["accuracy", "auc"]
+        assert fairness_metric in ["EOP", "SPD"]
+
+        self.fairness_constraint = fairness_constraint
+        self.dual_learning = dual_learning
+        self.multiplier_learning_rate = multiplier_learning_rate
+        self.fair_weight = fair_weight
+        self.n_estimators = n_estimators
+        self.eta = eta
+        self.colsample_bytree = colsample_bytree
+        self.max_depth = max_depth
+        self.min_child_weight = min_child_weight
+        self.max_leaves = max_leaves
+        self.l2_weight = l2_weight
+        self.alpha = alpha
+        self.performance_metric = performance_metric
+        self.fairness_metric = fairness_metric
+        self.random_state = random_state
+        self.group_losses = []
+        self.mu_opt_list = [None]
+
+    def fit(self, X, y, sensitive_attribute=None):
+        """Fit the model to the data.
+
+
+        Parameters
+        ----------
+        X : pandas.DataFrame
+            Dataframe of shape (n_samples, n_features)
+        y : pandas.Series or numpy.ndarray
+            Labels array-like of shape (n_samples), must be (0 or 1)
+        sensitive_attribute : pandas.Series or numpy.ndarray
+            Sensitive attribute array-like of shape (n_samples)
+
+        Returns
+        -------
+        XtremeFair
+            Fitted model
+        """
+        if sensitive_attribute is None:
+            sensitive_attribute = np.ones(X.shape[0])
+        X, y = check_X_y(X, y)
+        self.classes_ = np.unique(y)
+        dtrain = xgb.DMatrix(X, label=y)
+
+        params = {
+            "tree_method": "hist",
+            "objective": "binary:logistic",
+            "eta": self.eta,
+            "colsample_bytree": self.colsample_bytree,
+            "max_depth": self.max_depth,
+            "min_child_weight": self.min_child_weight,
+            "max_leaves": self.max_leaves,
+            "lambda": self.l2_weight,
+        }
+        if self.random_state is not None:
+            params["seed"] = self.random_state
+
+        self.model_ = xgb.train(
+            params,
+            dtrain,
+            num_boost_round=self.n_estimators,
+            obj=dual_obj_1(
+                sensitive_attribute,
+                self.fair_weight,
+                self.group_losses,
+                self.mu_opt_list,
+                self.fairness_constraint,
+                self.dual_learning,
+                self.multiplier_learning_rate,
+            ),
+        )
+        self.group_losses = np.array(self.group_losses)
+        self.mu_opt_list = np.array(self.mu_opt_list)
+        return self
+
+    def predict(self, X):
+        """Predict the labels of the data."""
+        check_is_fitted(self)
+        X = check_array(X)
+        dtest = xgb.DMatrix(X)
+        preds = self.model_.predict(dtest)
+        return (preds > 0.5).astype(int)
+
+    def predict_proba(self, X):
+        """Predict the probabilities of the data."""
+        check_is_fitted(self)
+        X = check_array(X)
+        dtest = xgb.DMatrix(X)
+        preds_pos = self.model_.predict(dtest)
+        preds = np.ones((preds_pos.shape[0], 2))
+        preds[:, 1] = preds_pos
+        preds[:, 0] -= preds_pos
+        return preds
+
+    def fairness_score(self, X, y, preds):
+        """Calculate the fairness score of the model. The sensitive attribute must be in the first column of X."""
+        A = X[:, 0].reshape(-1)
+        if self.fairness_metric == "EOP":
+            fair = np.mean(preds[(A == 1) & (y == 1)]) - np.mean(
+                preds[(A == 0) & (y == 1)]
+            )
+        elif self.fairness_metric == "SPD":
+            fair = np.mean(preds[A == 1]) - np.mean(preds[A == 0])
+        return 1 - np.abs(fair)
+
+    def score(self, X, y, return_type="combined"):
+        """
+        Calculate the performance-fairness score of the model. The score has the following formula:
+
+        $score = $perf * \alpha + (1 - |fair|) * (1 - \alpha)$$
+
+        where perf is the performance metric (accuracy or AUC), fair is the fairness metric (EOD or SPD), and alpha is the weight of the performance metric.
+        """
+        check_is_fitted(self)
+        X = check_array(X)
+        dtest = xgb.DMatrix(X)
+        preds = self.model_.predict(dtest)
+        if self.performance_metric == "auc":
+            perf = roc_auc_score(y, preds)
+        elif self.performance_metric == "accuracy":
+            ts = ks_threshold(y, preds)
+            perf = accuracy_score(y, preds > ts)
+        if self.alpha == 1:
+            return perf
+        fair = self.fairness_score(X, y, preds)
+        combined_score = perf * self.alpha + (1 - self.alpha) * fair
+        if return_type == "combined":
+            return combined_score
+        elif return_type == "performance":
+            return perf
+        elif return_type == "fairness":
+            return fair
+        else:
+            raise ValueError(
+                "Invalid return_type. Choose 'combined', 'performance', or 'fairness'."
+            )
+
 
 def ks_threshold(y_true, y_score):
     """Identify the threshold that maximizes the Kolmogorov-Smirnov statistic."""
