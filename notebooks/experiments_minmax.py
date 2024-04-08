24a5a712af7c5e07c38b852002ad9a0bb6fcac8c
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2024-04-08 01:41:25 +0000

    run again experiments with more trials and 5 folds

--- notebooks/experiments_minmax.py
+++ notebooks/experiments_minmax.py
@@ -9,14 +9,15 @@ fairness_metric = "min_bal_acc"
 thresh = "ks"
 
 for dataset in ["german", "adult"]:
-    for alpha in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
-        for model_name in ["M2FGB_grad"]:
+    for alpha in [0, 0.2, 0.4, 0.6, 0.8, 1]:
+        for model_name in ["M2FGB", "M2FGB_grad", "FairGBMClassifier"]:
             args = {
                 "dataset": dataset,
                 "alpha": alpha,
-                "output_dir": f"../results/comparing_metrics/{dataset}/{model_name}_{alpha}_{fairness_metric}",
+                "output_dir": f"../results/comparing_metrics_2/{dataset}/{model_name}_{alpha}_{fairness_metric}",
                 "model_name": model_name,
-                "n_trials": 10,
+                "n_trials": 100,
+                "n_folds" : 5,
                 "n_groups": 4,
                 "thresh" : thresh,
                 "fairness_metric": fairness_metric,
