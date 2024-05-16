41e6e52045304eb316bbd8e9579c5af28bf3f910
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2024-05-15 23:29:30 -0300

    small fix for m2fgb spd

--- scripts/experiments_new.py
+++ scripts/experiments_new.py
@@ -83,7 +83,7 @@ def get_model(model_name, random_state=None):
 
         def model(**params):
             return models.M2FGB(
-                dual_learning="gradient",
+                dual_learning="gradient_norm",
                 fairness_constraint="demographic_parity",
                 random_state=random_state,
                 **params,
