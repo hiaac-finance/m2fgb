fix: bug on gradient and hessian calculation

scripts/models.py
@@ -161,10 +161,10 @@ def logloss_group_grad(y_pred, y_true, fairness_constraint):
     """Create an array with the gradient of fairness metrics."""
     if fairness_constraint == "equalized_loss":
         grad = -(y_true - y_pred)
-    elif fairness_constraint == "true_positive_rate":
+    elif fairness_constraint == "positive_rate":
         y_ = np.ones(y_true.shape[0])  # all positive class
         grad = -(y_ - y_pred)
-    elif fairness_constraint == "equal_opportunity":
+    elif fairness_constraint == "true_positive_rate":
         grad = -(y_true - y_pred)
         grad[y_true == 0] = 0  # only consider the loss of the positive class
     elif fairness_constraint == "true_negative_rate":
@@ -176,12 +176,12 @@ def logloss_group_grad(y_pred, y_true, fairness_constraint):
 
 def logloss_group_hess(y_pred, y_true, fairness_constraint):
     """Create an array with the hessian of fairness metrics."""
-    if fairness_constraint == "equalized_loss":
-        hess = y_pred * (1 - y_pred)
-    elif (
-        fairness_constraint == "positive_rate"
-        or fairness_constraint == "true_positive_rate"
+    if (
+        fairness_constraint == "equalized_loss"
+        or fairness_constraint == "positive_rate"
     ):
+        hess = y_pred * (1 - y_pred)
+    elif fairness_constraint == "true_positive_rate":
         hess = y_pred * (1 - y_pred)
         hess[y_true == 0] = 0  # only consider the loss of the positive class
     elif fairness_constraint == "true_negative_rate":

scripts/utils.py
@@ -381,10 +381,10 @@ def logloss_group(y_ground, y_prob, A, fairness_constraint):
     y_prob = np.clip(y_prob, eps, 1 - eps)
     if fairness_constraint == "equalized_loss":
         loss = -(y_ground * np.log(y_prob) + (1 - y_ground) * np.log(1 - y_prob))
-    if fairness_constraint == "demographic_parity":
+    if fairness_constraint == "positive_rate":
         y_ = np.ones(y_ground.shape[0])  # all positive class
         loss = -(y_ground * np.log(y_prob) + (1 - y_ground) * np.log(1 - y_prob))
-    elif fairness_constraint == "equal_opportunity":
+    elif fairness_constraint == "true_positive_rate":
         loss = -(y_ground * np.log(y_prob) + (1 - y_ground) * np.log(1 - y_prob))
         loss[y_ground == 0] = 0  # only consider the loss of the positive class
 
@@ -392,7 +392,7 @@ def logloss_group(y_ground, y_prob, A, fairness_constraint):
     return loss
 
 
-def max_logloss_score(y_ground, y_prob, A):
+def max_logloss_score(y_ground, y_prob, A, fairness_constraint = "equalized_loss"):
     """Calculate the minimum mean loss of the groups. The loss is binary cross entropy.
     It work with multiple groups.
 
@@ -410,7 +410,7 @@ def max_logloss_score(y_ground, y_prob, A):
     float
         Minimum mean loss of groups
     """
-    logloss = logloss_group(y_ground, y_prob, A, "equalized_loss")
+    logloss = logloss_group(y_ground, y_prob, A, fairness_constraint)
     return max(logloss)
 
 