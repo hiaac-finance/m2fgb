9fa8c12cdd08192e65786f64a0fa25e4bae66568
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2025-07-01 14:53:01 -0300

    del: cleaning files

--- MMPF/MinimaxParetoFair/network/__init__.py
+++
@@ -1,7 +0,0 @@
-from .network_utils import *
-from .network_bodies import *
-from .network_heads import *
-import sys
-sys.path.append(".")
-sys.path.append("..")
-from MMPF.MinimaxParetoFair import *
